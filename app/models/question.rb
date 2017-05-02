bbe93b64380e573475930b79a0951d117d660a8a
olivia@ruiz-knott.com <olivia@ruiz-knott.com>
2017-05-02 11:52:09 -0400

    Added passcode column to quesitons table, added questions to seed file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :options
-  
+
   validates :question_text, presence: true
+  validates :passcode, numericality: { only_integer: true }, length: { is: 4 }
 end
